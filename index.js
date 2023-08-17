4891efddd4da346342b8cac8c364939eebcd5aa5
kylehek <kylehek99@gmail.com>
2023-08-17 16:12:30 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,14 @@
+// nav bar cart code
+// const cartNumber = document.getElementById('cart-number');
+// const addToCartButtons = document.getElementsByClassName('shop-item-button');
+
+// let counter = 0;
+// cartNumber.innerText = counter;
+
+// for (let i = 0; i < addToCartButtons.length; i++) {
+//     addToCartButtons[i].addEventListener('click', () => {
+//         counter++;
+//         cartNumber.innerText = counter;
+//     })
+// }
+
